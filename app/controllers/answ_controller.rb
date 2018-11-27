700c7950bb498b62c371128eab77eca64f122409
oleshko <evgeniy-oleshko@lenta.ru>
2018-11-27 20:53:16 +0200

    add POST req - 400 error

--- app/controllers/answ_controller.rb
+++ app/controllers/answ_controller.rb
@@ -44,9 +44,12 @@ class AnswController < ApplicationController
       require "net/http"
       require "uri"
 
-      uri = URI.parse("https://api.judge0.com/submissions/?base64_encoded=false&wait=false/")
+=begin
+    #  uri = URI.parse("https://api.judge0.com/submissions/?base64_encoded=false&wait=false/")
 
-# Shortcut
+
+
+    # Shortcut
     #response = Net::HTTP.post_form(uri, {"q" => "My query", "per_page" => "50"})
 
 # Full control
@@ -54,7 +57,7 @@ class AnswController < ApplicationController
 
     request = Net::HTTP::Post.new(uri.request_uri)
     request.set_form_data({"language_id" => "4","source_code" => "#include <stdio.h>\n\nint main(void) {\n  char name[10];\n  scanf(\"%s\", name);\n
-     #                                                       printf(\"hello, %s\n\", name);\n  return 0;\n)"})
+                                   printf(\"hello, %s\n\", name);\n  return 0;\n)"})
 
     response = http.request(request)
 
@@ -63,19 +66,33 @@ class AnswController < ApplicationController
      # @result = HTTParty.post(@urlstring_to_post.to_str,
      #                         :body => { "source_code": '#include <stdio.h>\n\nint main(void) {\n  char name[10];\n  scanf(\"%s\", name);\n
      #                                                       printf(\"hello, %s\n\", name);\n  return 0;\n}',
-      #                                   "language_id": 4,
-#
-      #                        }.to_json)
+      #                                   "language_id": 4}.to_json)
 
+=end
+    uri = URI("https://api.judge0.com/submissions/?base64_encoded=false&wait=false/")
 
+    req = Net::HTTP::Post.new(uri)
+    req.set_form_data('language_id'=>'4','source_code'=>'#include <stdio.h>\n\nint main(void) {\n  char name[10];\n  scanf(\"%s\", name);\n
+                                   printf(\"hello, %s\n\", name);\n  return 0;\n')
+    res =Net::HTTP.start(uri.hostname,uri.port) do |http|
+      http.request(req)
+    end
+
+    case res
+    when Net::HTTPSuccess, Net::HTTPRedirection
+      #OK
+
+    else
+
+      #res.value
 
       puts "--------------------------------------------------------------------------------------------------------------------------------------------------------------"
 
-      puts response
+      puts res
 
       puts "--------------------------------------------------------------------------------------------------------------------------------------------------------------"
 
-
+      end
 
       redirect_to root_path
       #debugger
@@ -83,6 +100,6 @@ class AnswController < ApplicationController
 
       #flash.now[:danger] = 'Invalid combination'
       #render 'new'
-  #  end
+    end
   end
-end
+#end
